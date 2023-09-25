948cc80c790975480dce423a3cecf3ba1df34f38
hallgato <marcell,lenkei@gmail.com>
2023-09-25 12:35:24 +0200

    initial commit

--- src/main/kotlin/hu/obuda/devops/fibonaccirestapi/controller/FibonacciController.kt
+++ src/main/kotlin/hu/obuda/devops/fibonaccirestapi/controller/FibonacciController.kt
@@ -2,6 +2,8 @@ package hu.obuda.devops.fibonaccirestapi.controller
 
 import hu.obuda.devops.fibonaccirestapi.service.FibonacciService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -14,9 +16,16 @@ class FibonacciController {
     @Autowired
     var fibonacciService: FibonacciService? = null
 
-    @GetMapping(value = ["fibonacci"])
-    open fun fibonacci(@RequestParam n: Int): Int? {
-        // TODO - If n is greater than 46 then return BAD REQUEST use HttpStatus
-        return fibonacciService?.fibonacci(n)
+    @GetMapping(value = ["/fibonacci"])
+    fun fibonacci(@RequestParam n: Int): ResponseEntity<Any> {
+        if (n > 46) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Input value n is too large")
+        }
+        return try {
+            val result = fibonacciService?.fibonacci(n)
+            ResponseEntity.ok(result)
+        } catch (e: Exception) {
+            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while calculating the Fibonacci number.")
+        }
     }
 }
--- src/main/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciService.kt
+++ src/main/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciService.kt
@@ -5,7 +5,25 @@ import org.springframework.stereotype.Service
 @Service
 class FibonacciService {
     fun fibonacci(n: Int): Int {
-        return if (n == 1) 0
-        else 0 // TODO instead of this logic implement fibonacci
+        return when (n) {
+            0 -> 0
+            1 -> 1
+            else -> {
+                var a = 0
+                var b = 1
+                var result = 0
+                for (i in 2..n) {
+                    result = a + b
+                    a = b
+                    b = result
+                }
+                result
+            }
+        }
     }
 }
+
+
+
+
+
